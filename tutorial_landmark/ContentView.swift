f3713ef2d112005d3ee3996c3a57b6da66cc8559
jaeson-dev <jaeson.dev@gmail.com>
2022-02-24 21:31:49 +0900

    Initial Commit

---
+++ tutorial_landmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorial_landmark
+//
+//  Created by Jaeson.dev on 2022/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tutorial_landmark/tutorial_landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorial_landmarkApp.swift
+//  tutorial_landmark
+//
+//  Created by Jaeson.dev on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct tutorial_landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
